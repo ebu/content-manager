7afdbe84c0a28655410f8147af9cc91050ff12f0
Mathieu Habegger <mh@swissmediapartners.ch>
2014-08-05 15:00:22 +0200

    Added static content

---
+++ io.ebu.eis.canvasgenerator/HTMLRenderer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace io.ebu.eis.canvasgenerator
+{
+    public class HTMLRenderer
+    {
+        private string _pathToExe = "phantomjs.exe";
+        private string _pathToWorkingDir = "templates/";
+        private string _phantomPageGenerator = "../sliderenderer.js";
+        private string _phantomHtmlGenerator = "../htmlrenderer.js";
+        private string _phantomArgumentProperties = "320px*240px";
+        private int _timeToExit = 500;
+
+        public HTMLRenderer(string templateDirectory)
+        {
+            _pathToWorkingDir = templateDirectory;
+        }
+
+        public BitmapImage Render(string file)
+        {
+            var args = String.Format("{0} {1} {2}", _phantomPageGenerator, file, _phantomArgumentProperties);
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = _pathToExe,
+                Arguments = args,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                WorkingDirectory = _pathToWorkingDir // PDF Tool Path
+            };
+            var p = new Process();
+            p.StartInfo = startInfo;
+            p.Start();
+            p.WaitForExit(_timeToExit);
+            //Read the Error:
+            //string error = p.StandardError.ReadToEnd();
+            //Read the Output:
+            var base64image = p.StandardOutput.ReadToEnd().Trim();
+            var bytes = Convert.FromBase64CharArray(base64image.ToCharArray(), 0, base64image.Length);
+            var image = GetBitmapImage(bytes);
+            return image;
+        }
+
+        public BitmapImage RenderHtml(string html)
+        {
+            // TODO Pass as argument and decode
+            var tempFilename = Guid.NewGuid().ToString()+".html";
+            var tempfile = Path.Combine(_pathToWorkingDir, tempFilename);
+            var file = File.Create(tempfile);
+            var content = Encoding.UTF8.GetBytes(html);
+            file.Write(content, 0, content.Length);
+            file.Flush();
+            file.Close();
+            //var htmlEncoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(html));
+            //var args = String.Format("{0} {1} {2}", _phantomHtmlGenerator, htmlEncoded, _phantomArgumentProperties);
+            //var startInfo = new ProcessStartInfo
+            //{
+            //    FileName = _pathToExe,
+            //    Arguments = args,
+            //    UseShellExecute = false,
+            //    CreateNoWindow = true,
+            //    RedirectStandardOutput = true,
+            //    RedirectStandardError = true,
+            //    RedirectStandardInput = true,
+            //    WorkingDirectory = _pathToWorkingDir // PDF Tool Path
+            //};
+            //var p = new Process();
+            //p.StartInfo = startInfo;
+            //p.Start();
+            //p.WaitForExit(_timeToExit);
+            ////Read the Error:
+            ////string error = p.StandardError.ReadToEnd();
+            ////Read the Output:
+            //var base64image = p.StandardOutput.ReadToEnd().Trim();
+            //var bytes = Convert.FromBase64CharArray(base64image.ToCharArray(), 0, base64image.Length);
+            //var image = GetBitmapImage(bytes);
+
+            // TODO This is ugly but works : )
+            var image = Render(System.IO.Path.GetFileName(tempfile));
+            
+            // TODO Remove
+            // Delete Temp file
+            File.Delete(tempfile);
+
+            return image;
+        }
+
+        public static BitmapImage GetBitmapImage(byte[] imageBytes)
+        {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(imageBytes);
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
+
+
+    }
+}
---
+++ io.ebu.eis.canvasgenerator/htmlrenderer.js
@@ -0,0 +1,60 @@
+﻿var page = require('webpage').create(),
+    system = require('system'),
+    size;
+
+if (system.args.length < 2 || system.args.length > 4) {
+    console.log('Usage: htmlrenderer.js HTMLBase64 viewportSize320px*240px zoomfactor');
+    phantom.exit(1);
+} else {
+    
+    page.viewportSize = { width: 320, height: 240 };
+    if (system.args.length > 2 && system.args[2].substr(-2) === "px") {
+        size = system.args[2].split('*');
+        if (size.length === 2) {
+            pageWidth = parseInt(size[0], 10);
+            pageHeight = parseInt(size[1], 10);
+            page.viewportSize = { width: pageWidth, height: pageHeight };
+            page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
+        } else {
+            console.log("size:", system.args[2]);
+            pageWidth = parseInt(system.args[2], 10);
+            pageHeight = parseInt(pageWidth * 3 / 4, 10); // it's as good an assumption as any
+            console.log("pageHeight:", pageHeight);
+            page.viewportSize = { width: pageWidth, height: pageHeight };
+        }
+    }
+    if (system.args.length > 3) {
+        page.zoomFactor = system.args[3];
+    }
+    
+    page.onLoadFinished = function () {
+        var base64image = page.renderBase64('PNG');
+        console.log(base64image);
+        phantom.exit();
+    };
+
+    //page.onResourceRequested = function (requestData, request) {
+    //    console.log('::loading', requestData['url']);  // this does get logged now
+    //};
+
+    page.settings.localToRemoteUrlAccessEnabled = true;
+    page.content = window.atob(system.args[1]);
+
+    //page.open("empty.html", function (status) { //about:blank
+        
+    //    if (status !== 'success') {
+    //        console.log('Unable to load the address!');
+    //        phantom.exit();
+    //    } else {
+            
+
+    //        window.setTimeout(function () {
+                
+    //            //page.render(output);
+    //            // TODO Check timeout ?
+               
+    //        }, 200);
+    //    }
+    //});
+    
+}
